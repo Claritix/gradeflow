7992a66914269d540e045872e72ce85fb4e47159
Claritix <126639506+Claritix@users.noreply.github.com>
2024-11-13 22:16:08 +0530

    Wrote app.py and layout.html begginings

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import os
 
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, world"
