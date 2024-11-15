06938e52064cb06cac933d54c9bbced20b9b2aaf
Claritix <126639506+Claritix@users.noreply.github.com>
2024-11-15 20:00:44 +0530

    wrote layout.html file, initlized routes and templates for other functions

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ from flask_session import Session
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] =  True
+
 @app.route('/')
-def hello():
-    return "Hello, world"
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/grades')
+def grades():
+    return render_template("grades.html")
+
+@app.route('/marks')
+def marks():
+    return render_template("marks.html")
+
+@app.route('/stats')
+def stats():
+    return render_template("stats.html")
+
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
