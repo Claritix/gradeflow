c323d9d93b26920542d88ad918c253a673a9b9ec
Claritix <126639506+Claritix@users.noreply.github.com>
2024-11-16 08:37:18 +0530

    db

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+# from flask_session import Session
 
 app = Flask(__name__)
 
